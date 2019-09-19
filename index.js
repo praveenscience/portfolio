613e7e2c918f766e567f3bd820c1bc5b95cfe18a
Ella <52179940+ehorn24@users.noreply.github.com>
2019-09-19 17:53:50 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(function() {
-  $(".typist")
+  var typ = $(".typist")
     .typist({
       speed: 12,
       text: "I AM AN ASPIRING FRONT-END DEVELOPER."
@@ -13,4 +13,28 @@ $(function() {
     .typistPause(2000)
     .typistRemove(21)
     .typistAdd("A SUGAR ADDICT.");
+  setInterval(() => {
+    typ
+      .typistPause(2000)
+      .typistRemove(15)
+      .typistAdd("AN ASPIRING FRONT-END DEVELOPER.")
+      .typistPause(2000)
+      .typistRemove(32)
+      .typistAdd("A WEB DESIGN ENTHUSIAST.")
+      .typistPause(2000)
+      .typistRemove(24)
+      .typistAdd("A PRETTY DECENT COOK.")
+      .typistPause(2000)
+      .typistRemove(21)
+      .typistAdd("A SUGAR ADDICT.");
+  }, 10000);
+  $(".navbar .links a").click(function(e) {
+    e.preventDefault();
+    $("html, body").animate(
+      {
+        scrollTop: $($(this).attr("href")).position().top - 100
+      },
+      1000
+    );
+  });
 });
